97c3b19d0a7b63ea1a605feddc1872712f7d4a22
test1 <18810955730@163.com>
2019-03-22 14:35:12 +0800

    add header

---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<home-header></home-header>
+	</div>
+	
+</template>
+<script >
+import HomeHeader from './componets/Header'
+export default{
+	name:'Home',
+	components:{
+		HomeHeader//es6中键和值相等可省略
+	}
+}
+	
+</script>
+<style >
+	
+</style>
