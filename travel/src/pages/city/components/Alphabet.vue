78391b09153977c57784496bf252c2aec231ad5c
test1 <18810955730@163.com>
2019-03-24 23:50:34 +0800

    finish city css

---
+++ travel/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<ul class="list">
+			<li class="item">A</li>
+			<li class="item">A</li>
+		</ul>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		name:'CityAlphabet'
+	}
+</script>
+<style type="text/css" lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+   display:flex
+   flex-direction:column
+   justify-content:center
+   position:absolute
+   top:1.58rem
+   right:0
+   bottom:0
+   width: .4rem
+   
+   .item
+      line-height:.4rem
+      text-alien:center
+      color:$bgColor
+</style>
